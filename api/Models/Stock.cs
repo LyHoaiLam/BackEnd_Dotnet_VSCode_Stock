e17c0395bbd8c4dd99b2497c86db734a3fc3103a
Lý Hoài Lâm <hoailam03999@gmail.com>
2024-05-02 13:17:53 +0700

    first commit

---
+++ api/Models/Stock.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema; // Thêm vào để sử dụng attribute Column
+
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id {get; set;}
+
+        public string Symbol {get; set;} = string.Empty;
+
+        public string CompanyName {get; set;} = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        
+        public decimal Purchase {get; set;}
+        [Column(TypeName = "decimal(18,2)")]
+
+        public decimal LastDiv {get; set;}
+
+        public string Industry {get; set;} = string.Empty;
+
+        public long MrketCap {get; set;}
+
+        public List<Comment> Comment {get; set;} = new List<Comment>();
+       
+    }
+}
+
+
